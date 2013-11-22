0f4df85682e817b6b399d87f89c39143dbdbd652
Bartendro <bartendro@nowhere.com>
2013-11-22 15:05:48 +0000

    Trandlate main page to spanish. Use Tea/Juice db. Add command to control indivual indios on the commandline

---
+++ bartendro/ui/indio/indioctl.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import sys
+import time
+import indio
+import serial
+import pololu
+
+# Connection to the Pi serial port
+ser=serial.Serial('/dev/ttyACM0', 56000, timeout=1)
+pol= pololu.Pololu(ser)
+
+indios = [None] * 8
+indios[0] = indio.Indio(pol,0,0)
+indios[1] = indio.Indio(pol,0,1)
+indios[2] = indio.Indio(pol,0,2)
+indios[3] = indio.Indio(pol,0,3)
+indios[4] = indio.Indio(pol,1,0)
+indios[5] = indio.Indio(pol,1,1)
+indios[6] = indio.Indio(pol,1,2)
+indios[7] = indio.Indio(pol,1,3)
+
+cmd = sys.argv[1]   
+indio = indios[int(sys.argv[2])]
+
+if cmd == "stand":
+    indio.stand()
+elif cmd == "sit":
+    indio.sit()
+elif cmd == "center":
+    indio.center()
+elif cmd == "left":
+    indio.left()
+elif cmd == "right":
+    indio.right()
+
+pol.wait_done()
+
+time.sleep(1)        
+print "Done"
